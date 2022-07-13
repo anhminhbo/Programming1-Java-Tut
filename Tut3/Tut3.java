d6533f7f0cafd8e02ec03fbfce0b600fc1223bf4
Minh Nguyens <wormscott12397@gmail.com>
2022-07-13 10:06:49 +0700

    update tut3, not finished

--- Tut3/Tut3.java
+++ Tut3/Tut3.java
@@ -1,5 +1,4 @@
 import java.util.*;
-import java.util.Scanner;
 
 public class Tut3 {
     //    Exercise 2
@@ -38,13 +37,63 @@ public class Tut3 {
     }
 
     //    Exercise 6
-    public static int[] sortBySelection (int[] numbers) {
+    public static void uniqueNumbers () {
+// Take the input from user
+       Scanner scanner = new Scanner(System.in);
+        //           + to have at least 1 maatch
+        String[] list = scanner.nextLine().split(" +");
+       while (true) {
+           boolean invalid = false;
+           for (String s:list) {
+               if (!s.matches("-?\\d")) {
+                   System.out.println("Please enter a valid list of valid integers");
+                   invalid = true;
+               }
+           }
 
-        for (int i= 0; i< numbers.length ; i++) {
-            numbers =  exchangeMinFrom(numbers,i);
-        }
+           if (invalid) continue;
 
-        return numbers;
+
+        HashMap<String,Integer> map = new HashMap<String, Integer>();
+
+           for (int i= 0; i< list.length ; i++) {
+               Integer freq = (Integer) map.get(list[i]);
+//               map.put(list[i], (freq == null) ? 1 : new Integer(freq.intValue() + 1));
+               map.put(list[i], (freq == null) ? 1 : freq + 1);
+
+           }
+
+           for (String key : map.keySet()) {
+               if (map.get(key) > 1)
+           }
+       }
+    }
+
+    //    Exercise 8
+    public static int[] regEx () {
+
+/*
+* a) 0 or 11 or 101
+  0|11|101
+
+ b) only 0s
+ 0*
+
+ c) all binary strings
+ (011)*
+
+ d) all binary strings except empty string
+ (011)(0|1)*
+
+ e) begins with 1, ends with 1
+ * 1|(001)*|1
+
+ f) ends with 00
+ (0|1)*00
+
+ g) contains at least three 1s
+
+ */
     }
 
     public static void main(String args[]) {
@@ -60,5 +109,7 @@ public class Tut3 {
         //        Ex 5
         System.out.println(Arrays.toString(sortBySelection(new int[]{0,1,6,5,4,3,3})));
 
+        //        Ex 6
+        System.out.println(Arrays.toString(uniqueNumbers()));
     }
 }
