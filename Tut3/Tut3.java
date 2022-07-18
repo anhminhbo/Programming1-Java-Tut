74fe1b23764854a05bea7a1a477f18840c539d7d
Minh Nguyens <wormscott12397@gmail.com>
2022-07-18 10:11:07 +0700

    update tut 3

--- Tut3/Tut3.java
+++ Tut3/Tut3.java
@@ -42,9 +42,10 @@ public class Tut3 {
        Scanner scanner = new Scanner(System.in);
         //           + to have at least 1 maatch
         String[] list = scanner.nextLine().split(" +");
+        HashMap<String, Integer> map = new HashMap<String, Integer>();
        while (true) {
            boolean invalid = false;
-           for (String s:list) {
+           for (String s : list) {
                if (!s.matches("-?\\d")) {
                    System.out.println("Please enter a valid list of valid integers");
                    invalid = true;
@@ -53,47 +54,153 @@ public class Tut3 {
 
            if (invalid) continue;
 
+           break;
+       }
 
-        HashMap<String,Integer> map = new HashMap<String, Integer>();
-
-           for (int i= 0; i< list.length ; i++) {
+           for (int i = 0; i < list.length; i++) {
                Integer freq = (Integer) map.get(list[i]);
 //               map.put(list[i], (freq == null) ? 1 : new Integer(freq.intValue() + 1));
                map.put(list[i], (freq == null) ? 1 : freq + 1);
 
            }
 
-           for (String key : map.keySet()) {
-               if (map.get(key) > 1)
+           for (String s: map.keySet()){
+               if (map.get(s) == 1){
+                   System.out.print(s + " ");
+               }
            }
-       }
+
     }
 
-    //    Exercise 8
-    public static int[] regEx () {
+        // Exercise 8
+    /*
+    a) 0 or 11 or 101
+    0 | 11 | 101
+    b) only 0s
+    0*
+    c) all binary strings
+    (0|1)*
+    d) all binary strings except empty string
+    (0|1)(0|1)*
+    e) begins with 1, ends with 1
+    1|(0|1)*|1
+    f) ends with 00
+    (0|1)*00
+    g) contains at least three 1s
+    (0|1)*1(0|1)*1(0|1)*1(0|1)*
+    h) contains at least three consecutive 1s
+    (0|1)*111(0|1)*
+    i) contains the substring 110
+    (0|1)*110(0|1)*
+    j) doesn't contain the substring 110
+    (0|10)*1*
+    k) has at least 3 characters, and the third character is 0
+    (0|1)(0|1)0(0|1)*
+    l) starts and ends with the same character
+    1(0|1)*1 | 0(0|1)*0
+    m) starts with 0 and has odd length, or starts with 1 and has even length
+    0((0|1)(0|1))* | 1(0|1)((0|1)(0|1))*
+    n) length is at least 1 and at most 3
+    (0|1)|(0|1)(0|1)|(0|1)(0|1)(0|1)
+     */
 
-/*
-* a) 0 or 11 or 101
-  0|11|101
+    // Exercise 7
+    public static void swapString(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the first string: ");
+        String firstString = scanner.next();
+        System.out.println("Enter the second string: ");
+        String secondString = scanner.next();
+        System.out.println("Strings before swap: 1st = " +
+                firstString + " and 2nd = "+ secondString);
+        firstString = firstString + secondString;
+        secondString = firstString.substring(0,firstString.length() -
+                secondString.length());
+        firstString = firstString.substring(secondString.length());
+        /*
+        firstString = firstString + secondString;
+        secondString = firstString.replace(secondString, "");
+        firstString = firstString.replace(secondString, "");
+         */
+        System.out.println("Strings after swap: 1st = " +
+                firstString + " and 2nd = "+ secondString);
+    }
 
- b) only 0s
- 0*
-
- c) all binary strings
- (011)*
-
- d) all binary strings except empty string
- (011)(0|1)*
-
- e) begins with 1, ends with 1
- * 1|(001)*|1
-
- f) ends with 00
- (0|1)*00
-
- g) contains at least three 1s
-
- */
+    // Exercise 9
+    public static void displayGame(char[][] board){
+        for (char[] row: board){
+            for (char cell: row){
+                System.out.print(cell);
+            }
+            System.out.println();
+        }
+    }
+    public static char endGame(char[][] board){
+        int BOARD_SIZE = 3;
+        char [] players = {'X', 'O'};
+        for (char player: players){
+            for (int row = 0; row < BOARD_SIZE; row++){
+                if (board[row][0] == player && board[row][1] == player &&
+                        board[row][2] == player)
+                    return player;
+            }
+        }
+        for (char player: players){
+            for (int col = 0; col < BOARD_SIZE; col++){
+                if (board[col][0] == player && board[col][1] == player &&
+                        board[col][2] == player)
+                    return player;
+            }
+        }
+        for (char player: players){
+            if (board[0][0] == player && board[1][1] == player && board[2][2] ==
+                    player)
+                return player;
+            if (board[0][2] == player && board[1][1] == player && board[2][0] ==
+                    player)
+                return player;
+        }
+        for(int row = 0; row < BOARD_SIZE; row++){
+            for(int col = 0; col < BOARD_SIZE; col++){
+                if (board[row][col] == '.'){
+                    return '.';
+                }
+            }
+        }
+        return ' ';
+    }
+    // Exercise 10
+    public static void playTicTacToe(){
+        char [] players = {'X', 'O'};
+        int currentPlayer = 0;
+        char[][] gameBoard = {
+                {'.','.','.'},
+                {'.','.','.'},
+                {'.','.','.'}
+        };
+        Scanner scanner = new Scanner(System.in);
+        char finish = endGame(gameBoard);
+        while (finish == '.'){
+            //game not finish , display the game board
+            displayGame(gameBoard);
+            // advance to the next player
+            currentPlayer = (currentPlayer + 1) % players.length;
+            System.out.println("Current player: " + players[currentPlayer]);
+            // get new piece from current Player
+            System.out.println("Choose row (0-2): ");
+            int row = scanner.nextInt();
+            System.out.println("Choose column (0-2): ");
+            int col = scanner.nextInt();
+            gameBoard[row][col] = players[currentPlayer];
+            //check game state
+            finish = endGame(gameBoard);
+        }
+        if (finish == ' '){
+            System.out.println("It's a tie");
+        }
+        else {
+            System.out.println(players[currentPlayer] + " wins!");
+        }
     }
 
     public static void main(String args[]) {
@@ -110,6 +217,11 @@ public class Tut3 {
         System.out.println(Arrays.toString(sortBySelection(new int[]{0,1,6,5,4,3,3})));
 
         //        Ex 6
-        System.out.println(Arrays.toString(uniqueNumbers()));
+        uniqueNumbers();
+
+//
+//        sortBySelection(array);
+//        printArray(array);
+        playTicTacToe();
     }
 }
