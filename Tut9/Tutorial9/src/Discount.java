6fb20a708e3b7307bf060ad73587d59c0ce75803
Minh Nguyens <wormscott12397@gmail.com>
2022-08-26 09:47:16 +0700

    update Tut9

---
+++ Tut9/Tutorial9/src/Discount.java
@@ -0,0 +1,11 @@
+public class Discount {
+    private String code;
+
+    public Discount(String code) {
+        this.code = code;
+    }
+
+    public double calculateDiscountedAmount(double sum){
+        return sum;
+    }
+}
